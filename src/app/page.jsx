94de2e6706bade243e2a48c5120f6048130b9c81
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-01-10 16:23:04 +0600

    Addded antd

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { Button } from "antd";
+
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Button type="dashed" danger>
+        Dashed
+      </Button>
     </div>
   );
 };
