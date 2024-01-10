1034cd2f46e0cf8bc23edf91736d6e59224a09b2
Meraj Hossain <merajhossain@Merajs-MacBook-Air.local>
2024-01-10 19:21:16 +0600

    Added form

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,24 +5,11 @@ import { Button } from "antd";
 
 const Home = () => {
   const [userLogin, { isLoading: loading }] = useUserLoginMutation();
-  const onSubmit = async () => {
-    try {
-      const res = await userLogin({
-        office_email: "employee@dino.com",
-        password: "Dino-123",
-      }).unwrap();
-      console.log(res);
-      message.success("User logged in successfully!");
-    } catch (err) {
-      console.error(err.message);
-      message.error(err.message);
-    }
-  };
 
   return (
     <div>
       <h1>Home Page</h1>
-      <Button onClick={() => onSubmit()} type="dashed" danger>
+      <Button type="dashed" danger>
         Dashed
       </Button>
     </div>
